d83335ded136138508e1f366cbf98b6873b7b69e
sivakorn <osacn_sivakorn.watcharavakhin@krungthai.com>
2023-04-16 20:10:13 +0700

    init commit

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Param } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
+  }
+}
